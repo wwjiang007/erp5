24017646b5c4ee6588f3869e3c04f573d3435df5
Tomas Peterka <tomas.peterka@nexedi.com>
2018-03-12 12:22:30 +0100

    [erp5_core] Rename useful Script *_getListbox according to API naming conventions

--- product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Form_getListbox.py
+++ product/ERP5/bootstrap/erp5_core/SkinTemplateItem/portal_skins/erp5_core/Form_getListbox.py
@@ -2,26 +2,30 @@
 # Christophe Dumez <christophe@nexedi.com>
 # This script should be used to detect a listbox without having to name it "listbox"
 
+def isListBox(field):
+  if field.meta_type == "ListBox":
+    return True
+  elif field.meta_type == "ProxyField":
+    template_field = field.getRecursiveTemplateField()
+    if template_field.meta_type == "ListBox":
+      return True
+  return False
+
+
 if form is None:
-  form=context
+  form = context
 
 if form.meta_type != 'ERP5 Form':
   return None
 
-# XXX We should not use meta_type properly,
-# XXX We need to discuss this problem.(yusei)
-def isListBox(field):
-  if field.meta_type=='ListBox':
-    return True
-  elif field.meta_type=='ProxyField':
-    template_field = field.getRecursiveTemplateField()
-    if template_field.meta_type=='ListBox':
-      return True
-  return False
+if form.has_field('listbox'):
+  return form.get_field('listbox')
 
 # we start with 'bottom' because most of the time
 # the listbox is there.
 for group in ('bottom', 'center', 'left', 'right'):
   for field in form.get_fields_in_group(group):
-     if isListBox(field) and not(field['hidden']) and field['enabled']:
-       return field
+     if (isListBox(field) and
+         not field.get_value('hidden') and
+         field.get_value('enabled')):
+        return field
