5b356a83ebe5e8dcb4f07191c7dcb70a123018bb
Cédric Le Ninivin <cedric.leninivin@nexedi.com>
2021-10-13 09:42:58 +0000

    erp5_upgrader: post upgrade alarm list all issues even when not solving

--- bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Base_postCheckConsistencyResult.py
+++ bt5/erp5_upgrader/SkinTemplateItem/portal_skins/erp5_upgrader/Base_postCheckConsistencyResult.py
@@ -1,14 +1,9 @@
-active_process = context.getPortalObject().restrictedTraverse(active_process)
-
-# We don't need check more if something is already inconsistent
-if active_process.ActiveProcess_sense() and not fixit:
-  return
-
 with context.defaultActivateParameterDict(activate_kw, placeless=True):
   constraint_message_list = context.checkConsistency(
     fixit=fixit, filter=filter_dict,)
 
-if constraint_message_list and not active_process.getResultList():
+if constraint_message_list:
+  active_process = context.getPortalObject().restrictedTraverse(active_process)
   active_process.postActiveResult(
     severity=0 if fixit else 1,
     summary="%s Consistency - At least one inconsistent object found" % ('Fix' if fixit else 'Check', ),
